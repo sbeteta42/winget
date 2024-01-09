bb0c0749da960cad4463edebacca761dc718967d
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2024-01-09 10:47:58 +0100

    Add files via upload

---
+++ Install_softs_winget.bat
@@ -0,0 +1,2 @@
+@echo - Install ation des Logiciels avec WINGET... Please wait !
+winget install Google.Chrome Mozilla.Firefox.ESR Mozilla.Thunderbird Notepad++.Notepad++ PuTTY.PuTTY WinSCP.WinSCP WiresharkFoundation.Wireshark 
